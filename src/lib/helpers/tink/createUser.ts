beb0f9061f61e04f1017c7b80e23f6360a65bd3b
Tsimafei Kremko <timkremko@gmail.com>
2026-02-14 11:41:37 +0100

    fix: reconnect bank account

--- src/lib/helpers/tink/createUser.ts
+++ src/lib/helpers/tink/createUser.ts
@@ -3,35 +3,38 @@ import { env } from "@/env";
 import { InternalServerError } from "@/lib/errors";
 
 export const createUser = async ({
-	appAccessToken,
-	userId,
-	market,
-	locale,
+  appAccessToken,
+  userId,
+  market,
+  locale,
 }: {
-	appAccessToken: string;
-	userId: string;
-	market: string;
-	locale: string;
+  appAccessToken: string;
+  userId: string;
+  market: string;
+  locale: string;
 }) => {
-	try {
-		const res = await axios.post(
-			`${env.TINK_BASE_URL}/api/v1/user/create`,
-			{
-				external_user_id: userId,
-				market,
-				locale,
-			},
-			{
-				headers: {
-					Authorization: `Bearer ${appAccessToken}`,
-					"Content-Type": "application/json",
-				},
-			},
-		);
-		return res.data;
-	} catch (error: any) {
-		throw new InternalServerError(
-			error.response?.data?.errorMessage || "Failed to create Tink user",
-		);
-	}
+  try {
+    const res = await axios.post(
+      `${env.TINK_BASE_URL}/api/v1/user/create`,
+      {
+        external_user_id: userId,
+        market,
+        locale,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${appAccessToken}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    return res.data;
+  } catch (error: any) {
+    if (error.response?.status === 409) {
+      return { external_user_id: userId };
+    }
+    throw new InternalServerError(
+      error.response?.data?.errorMessage || "Failed to create Tink user",
+    );
+  }
 };
