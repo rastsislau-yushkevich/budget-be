b22815118a5a3f86ff3ef171fbbf2722660682d0
Tsimafei Kremko <timkremko@gmail.com>
2026-02-14 11:20:55 +0100

    feature: disconnect bank account

--- src/controllers/tink/index.ts
+++ src/controllers/tink/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { authMiddleware } from "@/middlewares/auth";
 import { callback } from "./callback";
 import { connect } from "./connect";
+import { disconnectController } from "./disconnect";
 
 const router = express.Router();
 
 router.post("/connect", authMiddleware, connect);
 router.post("/callback", authMiddleware, callback);
+router.delete("/connect", authMiddleware, disconnectController);
 
 export default router;
